fadfcbbf4e33639b9cf951664a4edf1d8682fa41
Nounours31 <code.fages@gmail.com>
2023-04-11 00:12:25 +0200

    essai JavFX

---
+++ JavaNav/projet/app/src/main/java/sfa/nav/HelloFX.java
@@ -0,0 +1,30 @@
+package sfa.nav;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+	private static Logger logger = LoggerFactory.getLogger(HelloFX.class);
+	
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+    	logger.debug("ok logger - start the app !");
+        launch();
+    }
+
+}
---
+++ init.bat
@@ -0,0 +1,8 @@
+set JAVA_HOME=C:\paps\Programs\Java\java17
+set GRADLE_HOME=C:\paps\Programs\gradle-8.0.2
+set PATH=%GRADLE_HOME%;%GRADLE_HOME%/bin;%JAVA_HOME%;%JAVA_HOME%/bin;%PATH%
+set JAVAFX_HOME=C:\paps\Programs\Java\javaFX
+set PATH_TO_FX=%JAVAFX_HOME%\javafx-sdk-17.0.6\lib"
+set PATH_TO_FXJMOD=%JAVAFX_HOME%\javafx-jmods-17.0.6
+
+gradle
